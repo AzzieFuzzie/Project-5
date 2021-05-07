cd2f3ec4151d4caab9562ff34a2792fa5507b191
AzzieFuzzie <azziefuzzie01@gmail.com>
2021-05-07 09:13:13 +0200

    update all

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const searchContainer = document.querySelector(".search-container");
+const gallery = document.getElementById("gallery");
+const body = document.querySelector("body");
+
+fetch("https://randomuser.me/api/?results=12").then((response) =>
+  response.json().then((data) => profile(data.results))
+);
+
+function profile(data) {
+  for (let i = 0; i < data.length; i++) {
+    const galleryContent = `  <div class="card">
+<div class="card-img-container">
+    <img class="card-img" src=${data[i].picture.large} alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">${data[i].name.first}${data[i].name.last}</h3>
+    <p class="card-text">${data[i].email}</p>
+    <p class="card-text cap">${data[i].location.city}${data[i].location.state}</p>
+</div>
+</div>`;
+    gallery.insertAdjacentHTML("afterend", galleryContent);
+  }
+  const searchContent = `<form action="#" method="get">
+  <input type="search" id="search-input" class="search-input" placeholder="Search...">
+  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>`;
+  searchContainer.insertAdjacentHTML("afterend", searchContent);
+}
+
+gallery.addEventListener("click", (e) => {
+  const modal = `<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`;
+  body.insertAdjacentHTML("afterend", modal);
+});
