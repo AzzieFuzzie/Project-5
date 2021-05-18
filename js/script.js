88ccbc63dd09c94b88d80523fc7630073cd2b544
AzzieFuzzie <azziefuzzie01@gmail.com>
2021-05-18 09:36:53 +0200

    restarted

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ const searchContainer = document.querySelector(".search-container");
 const gallery = document.getElementById("gallery");
 const body = document.querySelector("body");
 
+// Fetches info from api async and passes data in function
+
 fetch("https://randomuser.me/api/?results=12").then((response) =>
   response.json().then((data) => {
     generateProfile(data.results);
-    modalProfile(data.results);
-    PopUp(data.results);
-    employeeModal(data.results);
+
     console.log(data.results);
   })
 );
+
+// Generates the profile with template literal using data from api
 
 function generateProfile(data) {
   for (let i = 0; i < data.length; i++) {
@@ -28,51 +30,10 @@ function generateProfile(data) {
   }
 }
 
-function modalProfile(data) {
-  modal = `<div class="modal-container">
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    </div> `;
-  gallery.insertAdjacentHTML("afterend", modal);
-  const modalContainer = document.querySelector(".modal-container");
-  const modalClose = document.querySelector("#modal-close-btn");
-  modalContainer.style.display = "none";
-  modalClose.addEventListener("click", (e) => {
-    modalContainer.style.display = "none";
-  });
-}
+const cards = document.querySelectorAll(".card");
+console.log(cards);
 
-function employeeModal(data) {
-  for (let i = 0; i < data.length; i++) {
-    `<div class="modal-info-container">
-  <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
-  <h3 id="name" class="modal-name cap">${data[i].name.first}</h3>
-  <p class="modal-text">${data[i].location.city}</p>
-  <hr>
-  <p class="modal-text">${data[i].cell}</p>
-  <p class="modal-text">${data[i].location.street}</p>
-  <p class="modal-text">${data[i].dob.date}</p>
-</div>
-`;
-  }
-  const infoContainer = (document.querySelectorAll(
-    "#modal-info-container"
-  ).innerHTML = " ");
-  const modalContainer = document.querySelector(".modal-container");
-  modalContainer.insertAdjacentHTML("afterbegin", infoContainer);
-}
-function PopUp(info) {
-  const cards1 = document.querySelectorAll(".card");
-  console.log(cards1);
-  for (let i = 0; i < cards1.length; i++) {
-    cards1[i].addEventListener("click", (e) => {
-      const modalContainer = document.querySelector(".modal-container");
-
-      modalContainer.style.display = "block";
-    });
-  }
-}
-
+// Template literal for search bar
 const searchContent = `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
