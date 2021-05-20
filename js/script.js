b6154cacd8c3152ab429b23c84699eda235c79eb
AzzieFuzzie <azziefuzzie01@gmail.com>
2021-05-20 16:26:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-const searchContainer = document.querySelector(".search-container");
-const gallery = document.getElementById("gallery");
-const body = document.querySelector("body");
+const searchContainer = document.querySelector('.search-container');
+const gallery = document.getElementById('gallery');
+const body = document.querySelector('body');
 
 // Fetches info from api async and passes data in function
 
-fetch("https://randomuser.me/api/?results=12").then((response) =>
-  response.json().then((data) => {
+fetch('https://randomuser.me/api/?results=12')
+  .then((response) => response.json())
+  .then((data) => {
     generateProfile(data.results);
     modal();
+    closeModal();
     console.log(data.results);
-  })
-);
+  });
 
 // Generates the profile with template literal using data from api
 
@@ -27,48 +28,55 @@ function generateProfile(data) {
 </div>
 </div>`;
 
-    gallery.insertAdjacentHTML("beforeend", galleryContent);
+    gallery.insertAdjacentHTML('beforeend', galleryContent);
   }
 }
 
 // Event listener for modal and then generates the modal
 
 function modal() {
-  gallery.addEventListener("click", (e) => {
-    const cards = document.querySelectorAll(".card");
-    console.log(cards);
-    if (e.target === cards) {
+  const cards = document.querySelectorAll('.card');
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', (e) => {
       const modal = `<div class="modal-container">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src="#" alt="profile picture">
-              <h3 id="name" class="modal-name cap"></h3>
-              <p class="modal-text">email</p>
-              <p class="modal-text cap">city</p>
-              <hr>
-              <p class="modal-text">(555) 555-5555</p>
-              <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-              <p class="modal-text">Birthday: 10/21/2015</p>
-          </div>
-          </div>
-      </div>`;
-      gallery.insertAdjacentHTML("afterbegin", modal);
-    }
-  });
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="#" alt="profile picture">
+                <h3 id="name" class="modal-name cap"></h3>
+                <p class="modal-text">email</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+            </div>
+        </div>`;
+      gallery.insertAdjacentHTML('afterbegin', modal);
+    });
+  }
 }
 // close modal
 
-gallery.addEventListener("click", (e) => {
-  const modalClose = document.querySelector("#modal-close-btn");
-  console.log(modalClose);
-  const modalContainer = document.querySelector(".modal-container");
-  console.log(modalContainer);
-  if (e.target === modalClose) modalContainer.style.display = "none";
-});
+function closeModal() {
+  const cards = document.querySelector('#gallery');
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', (e) => {
+      const modalClose = document.querySelector('#modal-close-btn');
+      console.log(modalClose);
+      const modalContainer = document.querySelector('.modal-container');
+      console.log(modalContainer);
+      if (e.target === modalClose) modalContainer.style.display = 'none';
+    });
+  }
+}
+
 // Template literal for search bar
 const searchContent = `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`;
-searchContainer.insertAdjacentHTML("afterEnd", searchContent);
+searchContainer.insertAdjacentHTML('afterEnd', searchContent);
